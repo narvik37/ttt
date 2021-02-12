e52e03fa51352ea730185acf49d76f4a03741c50
Kyuhyeon <narvik37@naver.com>
2021-02-12 22:44:19 +0900

    main writing..

--- tttmain.py
+++ tttmain.py
@@ -1,6 +1,6 @@
-import pygame, sys
-# from pygame.locals import *
-# from tttAI import *
+import pygame, sys, random
+from pygame.locals import *
+from tttAI import *
 
 # # Define the colors we will use in RGB format
 BLACK= ( 0,  0,  0)
@@ -28,6 +28,11 @@ center_x = width / 2
 center_y = height / 2
 box_size = 80
 text_size = 50
+
+top = center_y - box_size - box_size / 2
+down = top + box_size * 3
+left = center_x - box_size - box_size / 2
+right = left + box_size * 3
 
 def main():
     pygame.init()
@@ -59,7 +64,7 @@ class Menu(object):
         self.new_btn = self.make_text('New Game', BLACK, WHITE, x, height-30)
         x = center_x + width / 4
         self.quit_btn = self.make_text('Quit Game', BLACK, WHITE, x, height-30)
-    def result(self, msd_id):
+    def result(self, msg_id):
         msg = {
             'X' : 'YOU LOST',
             'O' : 'YOU WIN',
@@ -93,12 +98,53 @@ class Menu(object):
             clock.tick(fps)
 
 # TTT
+
+class TTT(object):
+    def __init__(self, screen, menu):
+        self.board = [['e' for i in range(3)] for j in range(3)]
+        self.coords = []
+        self.set_coords()
+        self.screen = screen
+        self.menu = menu
+        pass
+
+    def init_game(self):
+        self.ai = AI(self.board)
+        # self.draw_line(self.screen)
+        self.finish = False
+        self.init_board()
+        # self.set_first_player()
+
+    def init_board(self):
+        for y in range(3):
+            for x in range(3):
+                self.board[y][x] = empty
+
+    def set_coords(self):
+        for i in range(3):
+            for j in range(3):
+                coord = left + j * box_size, top + i * box_size
+                self.coords.append(coord)
+
+    def get_coord(self, pos):
+        for coord in self.coords:
+            x, y = coord
+            rect = pygame.Rect(x, y, box_size, box_size)
+            if(rect.collidepoint(pos)):
+                return coord
+        return None
+
 # run_game
+
+# def run_game(screen, menu, ttt):
+
+
 # terminate
 
 def terminate():
     pygame.quit()
     sys.exit()
+
 
 
 
